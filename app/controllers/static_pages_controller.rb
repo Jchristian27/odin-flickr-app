c0d26fcde068f72a74a2f519af8a8fc5c589dd63
Jonathan Christian <christianj707@gmail.com>
2024-03-12 18:11:24 -0700

    get form to post params correctly

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,20 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
   def index
+    if params[:user_id].present?
+      user_id = params[:user_id]
+
+      Flickr.api_key = ENV['FLICKR_API_KEY']
+      Flickr.shared_secret = ENV['FLICKR_SHARED_SECRET']
+
+      flickr = Flickr.new
+      begin
+        photostream = flickr.people.getPhotos(user_id: user_id)
+        @photos = photostream.map { |photo| photo.url }
+      rescue Flickr::FailedResponse => e
+        flash[:error] = "Error fetching photostream: #{e.message}"
+      end
+    end
   end
 end
